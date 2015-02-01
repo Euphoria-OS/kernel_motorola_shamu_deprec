ad4c587ddb057153b98a8577df4abfc9dd79f7f3
engstk <eng.stk@sapo.pt>
2015-02-01 03:54:33 +0000

    blu_plug: remove unused functions
    not available on shamu
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- arch/arm/mach-msm/blu_plug.c
+++ arch/arm/mach-msm/blu_plug.c
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2013 Stratos Karafotis <stratosk@semaphore.gr> (dyn_hotplug for mako)
  *
- * Copyright (C) 2014 engstk <eng.stk@sapo.pt> (hammerhead & shamu implementation, fixes and changes to blu_plug)
+ * Copyright (C) 2015 engstk <eng.stk@sapo.pt> (hammerhead & shamu implementation, fixes and changes to blu_plug)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
