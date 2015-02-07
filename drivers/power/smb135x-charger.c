944b1c5c1378e2984e679ef1f9dd1e5ff947016f
David Smit <dsmitty166@gmail.com>
2015-02-07 07:28:44 -0600

    Revert "Nexus 6: USB fastcharge"
    This reverts commit 8e3fc6c2f86463068d6f9802b57cfae1a4600194.

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -34,10 +34,6 @@
 static bool use_wlock = true;
 module_param(use_wlock, bool, 0644);
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-#include <linux/fastcharge.h>
-#endif
-
 #define SMB135X_BITS_PER_REG	8
 
 /* Mask/Bit helpers */
@@ -1566,11 +1562,6 @@ static int smb135x_set_appropriate_current(struct smb135x_chg *chip,
 	}
 
 	if (path == USB) {
-#ifdef CONFIG_FORCE_FAST_CHARGE
-		if (force_fast_charge)
-			path_current = USB_FASTCHARGE_CURRENT_LIMIT;
-		else
-#endif
 		path_current = chip->usb_psy_ma;
 		func = smb135x_set_usb_chg_current;
 	} else {
